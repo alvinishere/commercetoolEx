e8224bab816a2c6c06ba82a7e82febb837fa12e2
Alvin Pang <alvin_pang@epam.com>
2024-06-03 12:09:46 +0800

    initialized springboot application

---
+++ src/main/java/com/example/commercetoolEx/CommercetoolExApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercetoolEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercetoolExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercetoolExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/commercetoolEx/CommercetoolExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commercetoolEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommercetoolExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
